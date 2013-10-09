223edf4eca706f117cda02f7da792c4eaeb26ad0
Ryan Barrett <git@ryanb.org>
2013-10-08 20:43:13 -0700

    add dropbox and wordpress REST API (ie wordpress.com)

--- appengine_config.py
+++ appengine_config.py
@@ -1,7 +1,9 @@
 """App Engine settings.
 
-Reads Facebook and Twitter app keys and secrets into constants from these files:
+Reads app keys and secrets into constants from these files:
 
+dropbox_app_key
+dropbox_app_secret
 facebook_app_id
 facebook_app_secret
 facebook_app_id_local
@@ -14,6 +16,10 @@ instagram_client_id_local
 instagram_client_secret_local
 twitter_app_key
 twitter_app_secret
+wordpress_client_id
+wordpress_client_secret
+wordpress_client_id_local
+wordpress_client_secret_local
 """
 
 from __future__ import with_statement
@@ -48,12 +54,18 @@ if DEBUG:
   FACEBOOK_APP_SECRET = read('facebook_app_secret_local')
   INSTAGRAM_CLIENT_ID = read('instagram_client_id_local')
   INSTAGRAM_CLIENT_SECRET = read('instagram_client_secret_local')
+  WORDPRESS_CLIENT_ID = read('wordpress.com_client_id_local')
+  WORDPRESS_CLIENT_SECRET = read('wordpress.com_client_secret_local')
 else:
   FACEBOOK_APP_ID = read('facebook_app_id')
   FACEBOOK_APP_SECRET = read('facebook_app_secret')
   INSTAGRAM_CLIENT_ID = read('instagram_client_id')
   INSTAGRAM_CLIENT_SECRET = read('instagram_client_secret')
+  WORDPRESS_CLIENT_ID = read('wordpress.com_client_id')
+  WORDPRESS_CLIENT_SECRET = read('wordpress.com_client_secret')
 
+DROPBOX_APP_KEY = read('dropbox_app_key')
+DROPBOX_APP_SECRET = read('dropbox_app_secret')
 GOOGLE_CLIENT_ID = read('google_client_id')
 GOOGLE_CLIENT_SECRET = read('google_client_secret')
 TWITTER_APP_KEY = read('twitter_app_key')
