0b480e8cde10f2b0c61a95084da12ec44fd9843c
Ryan Barrett <git@ryanb.org>
2013-12-31 11:56:51 -0800

    default HTTP_HOST to localhost

--- appengine_config.py
+++ appengine_config.py
@@ -14,6 +14,6 @@ except AttributeError:
 # app_identity.get_default_version_hostname() would be better here, but
 # it doesn't work in dev_appserver since that doesn't set
 # os.environ['DEFAULT_VERSION_HOSTNAME'].
-HOST = os.getenv('HTTP_HOST')
+HOST = os.getenv('HTTP_HOST', 'localhost')
 SCHEME = 'https' if (os.getenv('HTTPS') == 'on') else 'http'
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
