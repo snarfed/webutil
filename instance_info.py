aff89c2016c9368018ae395e6ad694bf772e3d68
Ryan Barrett <git@ryanb.org>
2014-07-27 16:56:04 -0700

    add concurrent request recording to instance_info. for snarfed/bridgy#90

--- instance_info.py
+++ instance_info.py
@@ -8,14 +8,29 @@ be reported as 0.
 
 (The docs say it's deprecated because it's part of Backends, which is replaced
 by Modules, but I haven't found a corresponding part of the Modules API.)
+
+To turn on concurrent request recording, add this to your appengine_config.py:
+
+def webapp_add_wsgi_middleware(app):
+  from webutil import instance_info
+  app = instance_info.concurrent_requests_wsgi_middleware(app)
 """
 
+import datetime
 import os
 
 import appengine_config
+import collections
 from google.appengine.api import runtime
 import handlers
+import threading
 import webapp2
+
+Concurrent = collections.namedtuple('Concurrent', ('when', 'count'))
+
+# globals. stores Concurrents.
+concurrents = collections.deque(maxlen=20)
+
 
 class InfoHandler(handlers.TemplateHandler):
   def template_file(self):
@@ -23,7 +38,27 @@ class InfoHandler(handlers.TemplateHandler):
                         'templates/instance_info.html')
 
   def template_vars(self):
-    return {'os': os, 'runtime': runtime}
+    return {'concurrents': concurrents,
+            'os': os,
+            'runtime': runtime,
+            'threading': threading,
+            }
+
+
+def concurrent_requests_wsgi_middleware(app):
+  """WSGI middleware for per request instance info instrumentation.
+
+  Follows the WSGI standard. Details: http://www.python.org/dev/peps/pep-0333/
+  """
+  def wrapper(environ, start_response):
+    if threading.active_count() > 1:
+      # not thread safe!
+      global concurrents
+      concurrents.append(Concurrent(when=datetime.datetime.now(),
+                                    count=threading.active_count()))
+    return app(environ, start_response)
+
+  return wrapper
 
 
 application = webapp2.WSGIApplication([
