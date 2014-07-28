36a197ed7c7d3349cfaeac0255e64413ebd42487
Ryan Barrett <git@ryanb.org>
2014-07-28 09:08:23 -0700

    store concurrent requests in a heap so we keep the highest instances. for snarfed/bridgy#90

--- instance_info.py
+++ instance_info.py
@@ -16,22 +16,28 @@ def webapp_add_wsgi_middleware(app):
   app = instance_info.concurrent_requests_wsgi_middleware(app)
 """
 
+import collections
 import datetime
+import heapq
 import os
 
 import appengine_config
-import collections
 from google.appengine.api import runtime
 import handlers
 import threading
 import webapp2
 
-Concurrent = collections.namedtuple('Concurrent', ('when', 'count'))
+CONCURRENTS_SIZE = 20
+
+# A time when there was more than one request running at once. count is the
+# first field so that instances compare by count, since they're stored in a
+# heap, so that we keep the highest count instances.
+Concurrent = collections.namedtuple('Concurrent', ('count', 'when'))
 
 # globals
 current_requests = set()  # stores string request IDs
 current_requests_lock = threading.Lock()
-concurrents = collections.deque(maxlen=20)  # stores Concurrents; thread safe
+concurrents = []  # a heapq. stores Concurrents
 
 
 class InfoHandler(handlers.TemplateHandler):
@@ -59,8 +65,10 @@ def concurrent_requests_wsgi_middleware(app):
     with current_requests_lock:
       current_requests.add(req_id)
       if len(current_requests) > 1:
-        concurrents.append(Concurrent(when=datetime.datetime.now(),
-                                      count=len(current_requests)))
+        heapq.heappush(concurrents, Concurrent(count=len(current_requests),
+                                               when=datetime.datetime.now()))
+        if len(concurrents) > CONCURRENTS_SIZE:
+          heapq.heappop(concurrents)
 
     ret = app(environ, start_response)
 
