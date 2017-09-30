b91c787a732407c2b5950ed0c5d761bcedef23e6
Ryan Barrett <git@ryanb.org>
2017-09-30 07:50:53 -0700

    appengine_config.py: switch to app_identity.get_default_version_hostname()
    evidently it works in dev_appserver now.

--- appengine_config.py
+++ appengine_config.py
@@ -5,16 +5,17 @@ import os
 
 HTTP_TIMEOUT = 15  # seconds
 
+SCHEME = 'https' if (os.getenv('HTTPS') == 'on') else 'http'
+
 try:
   from google.appengine.api import app_identity
   APP_ID = app_identity.get_application_id()
+  HOST = app_identity.get_default_version_hostname()
 except (ImportError, AttributeError):
   # this is probably a unit test
   APP_ID = None
+  HOST = os.getenv('HTTP_HOST', 'localhost')
 
-# app_identity.get_default_version_hostname() would be better here, but
-# it doesn't work in dev_appserver since that doesn't set
-# os.environ['DEFAULT_VERSION_HOSTNAME'].
-HOST = os.getenv('HTTP_HOST', 'localhost')
-SCHEME = 'https' if (os.getenv('HTTPS') == 'on') else 'http'
+HOST_URL = '%s://%s' % (SCHEME, HOST)
+
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
