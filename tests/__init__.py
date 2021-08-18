af6fdc40874a995c271f089a1b4c1ab7fe75a6bb
Ryan Barrett <git@ryanb.org>
2021-08-18 08:09:16 -0700

    tweak test logging config

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,11 @@
 import logging, sys
 
+logging.getLogger('chardet').setLevel(logging.INFO)
+logging.getLogger('google.cloud').setLevel(logging.INFO)
+logging.getLogger('urllib3').setLevel(logging.INFO)
+
 # Piggyback on unittest's -v and -q flags to show/hide logging.
 if 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
   logging.disable(logging.CRITICAL + 1)
 elif '-v' in sys.argv:
   logging.getLogger().setLevel(logging.DEBUG)
-  logging.getLogger('google.cloud').setLevel(logging.INFO)
-  logging.getLogger('urllib3').setLevel(logging.INFO)
