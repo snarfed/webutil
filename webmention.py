f2b421b0ae5143d2aa2c51a7dcfb26485674fa06
Ryan Barrett <git@ryanb.org>
2022-10-26 09:25:11 -0700

    webmention.send logging, don't emit None for missing Location header

--- webmention.py
+++ webmention.py
@@ -112,6 +112,6 @@ def send(endpoint, source, target, **requests_kwargs):
     logger.debug(f'webmention send: got {e.__class__.__name__}')
     raise
 
-  logger.debug(f'webmention send: got HTTP {resp.status_code} {resp.headers.get("Location")}')
+  logger.debug(f'webmention send: got HTTP {resp.status_code} {resp.headers.get("Location", "")}')
   resp.raise_for_status()
   return resp
