eead4f98dc48b1bd59aabe5897efcca350fc699b
Ryan Barrett <git@ryanb.org>
2022-11-13 08:24:27 -0800

    appengine_info: support running against prod datastore
    with $GOOGLE_APPLICATION_CREDENTIALS

--- appengine_info.py
+++ appengine_info.py
@@ -2,9 +2,22 @@
 
 App Engine: https://cloud.google.com/appengine/docs/standard/python3/runtime#environment_variables
 WSGI: https://www.python.org/dev/peps/pep-3333/
-"""
-import os
 
-APP_ID = os.getenv('GAE_APPLICATION', '').split('~')[-1]
-DEBUG = os.environ.get('GAE_ENV') in (None, 'localdev')  # 'standard' in production
-LOCAL = os.environ.get('GAE_ENV') == 'localdev'
+To run against the prod datastore, make a service account, download its JSON
+credentials file, then run eg:
+
+env GOOGLE_APPLICATION_CREDENTIALS=service_account_creds.json FLASK_ENV=development flask run -p 8080
+"""
+import os, sys
+
+creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+if creds and not creds.endswith('fake_user_account.json'):
+  print(f'Found GOOGLE_APPLICATION_CREDENTIALS {creds}')
+  print('!!! Starting in production mode !!!')
+  APP_ID = 'bridgy-federated'
+  DEBUG = False
+  LOCAL = True
+else:
+  APP_ID = os.getenv('GAE_APPLICATION', '').split('~')[-1]
+  DEBUG = os.environ.get('GAE_ENV') in (None, 'localdev')  # 'standard' in production
+  LOCAL = os.environ.get('GAE_ENV') == 'localdev'
