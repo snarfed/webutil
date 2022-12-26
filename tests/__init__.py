0a5dc98e1cf0345a69f4ffc9a51931910d8c44e3
Ryan Barrett <git@ryanb.org>
2022-12-26 13:21:40 -0800

    unit test command line: make -v override discover

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,7 +13,7 @@ logging.getLogger('urllib3').setLevel(logging.INFO)
 
 # Piggyback on unittest's -v and -q flags to show/hide logging.
 logging.basicConfig()
-if 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
-  logging.disable(logging.CRITICAL + 1)
-elif '-v' in sys.argv:
+if '-v' in sys.argv:
   logging.getLogger().setLevel(logging.DEBUG)
+elif 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
+  logging.disable(logging.CRITICAL + 1)
