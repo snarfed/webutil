c5cf0610643f4a33455aad6e58d85b6b16fb58c4
Ryan Barrett <git@ryanb.org>
2023-02-24 07:07:46 -0600

    add fork of ndb's JsonProperty that works in the datastore web console
    https://github.com/googleapis/python-ndb/issues/874#issuecomment-1442753255

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-"""App Engine datastore model base classes and utilites.
+"""App Engine datastore model base classes, properties, and utilites.
 """
 from google.cloud import ndb
+
+from oauth_dropins.webutil.util import json_dumps, json_loads
 
 
 class StringIdModel(ndb.Model):
@@ -9,3 +11,24 @@ class StringIdModel(ndb.Model):
     """Raises AssertionError if string id is not provided."""
     assert self.key and self.key.string_id(), 'string id required but not provided'
     return super(StringIdModel, self).put(*args, **kwargs)
+
+
+class JsonProperty(ndb.TextProperty):
+    """Fork of ndb's that subclasses TextProperty instead of BlobProperty.
+
+    This makes values show up as normal, human-readable, serialized JSON in the
+    web console.
+    https://github.com/googleapis/python-ndb/issues/874#issuecomment-1442753255
+    """
+    def _validate(self, value):
+        if not isinstance(value, dict):
+            raise TypeError("JSON property must be a dict")
+
+    def _to_base_type(self, value):
+        as_str = json_dumps(value, separators=(",", ":"), ensure_ascii=True)
+        return as_str.encode("ascii")
+
+    def _from_base_type(self, value):
+        if not isinstance(value, str):
+            value = value.decode("ascii")
+        return json_loads(value)
