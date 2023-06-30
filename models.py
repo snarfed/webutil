280a93e092f3f2145e7b2ff2da7f7d9b20054224
Ryan Barrett <git@ryanb.org>
2023-06-29 21:27:29 -0700

    models: add *SetProperty that de-dupe repeated elements

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 """App Engine datastore model base classes, properties, and utilites.
 """
+from collections import UserList
+
 from google.cloud import ndb
+from google.cloud.ndb.exceptions import BadValueError
 
 from oauth_dropins.webutil.util import json_dumps, json_loads
 
@@ -43,3 +46,93 @@ class ComputedJsonProperty(JsonProperty, ndb.ComputedProperty):
     def __init__(self, *args, **kwargs):
         kwargs['indexed'] = False
         super().__init__(*args, **kwargs)
+
+
+class Unique:
+  """ndb Property mixin class that acts like a set instead of a list.
+
+  Specifically, de-duplicates elements with :class:`UniqueList` so that each
+  distinct value appears at most once.
+  """
+  def __init__(self, *args, **kwargs):
+    assert 'repeated' not in kwargs, f'{self.__class__} is always repeated'
+    super().__init__(*args, repeated=True, **kwargs)
+
+  def _store_value(self, entity, value):
+    return super()._store_value(entity, UniqueList(value))
+
+
+class StringSetProperty(Unique, ndb.StringProperty):
+  """StringProperty that's repeated and de-duplicates elements."""
+
+
+class KeySetProperty(Unique, ndb.KeyProperty):
+  """KeyProperty that's repeated and de-duplicates elements."""
+
+
+class UniqueList(UserList):
+  """A list-compatible class that de-duplicates elements.
+
+  Specifically, if an element is already in a UniqueList, inserting or appending
+  it again has no effect.
+
+  Not intended for large lists! Most operations are quadratic, ie O(n^2).
+
+  Used in :class:`StringSetProperty`.
+  """
+  def __init__(self, val=None):
+    super().__init__(val)
+    self.dedupe()
+
+  def append(self, val):
+    super().append(val)
+    self.dedupe()
+
+  add = append
+
+  def extend(self, val):
+    super().extend(val)
+    self.dedupe()
+
+  update = extend
+
+  def insert(self, i, val):
+    super().insert(i, val)
+    self.dedupe()
+
+  def __setitem__(self, key, val):
+    super().__setitem__(key, val)
+    self.dedupe()
+
+  def __add__(self, val):
+    return UniqueList(super() + val)
+
+  def __iadd__(self, val):
+    super().__iadd__(val)
+    self.dedupe()
+    return self
+
+  def dedupe(self):
+    deduped = []
+
+    # preserve order; don't use set or dict since ndb _BaseValue isn't hashable
+    for elem in self.data:
+      if elem not in deduped:
+        deduped.append(elem)
+
+    self.data = deduped
+
+
+class AssignableSet(set):
+  """A set-compatible class that allows assigning to a slice of the whole set.
+
+  Eg this class allows x[:] = [...], which replaces the entire contents.
+  """
+  def __setitem__(self, key, val):
+    if key == slice(None):
+      val = set(val)
+      self.clear()
+      self.update(val)
+      return
+
+    return super().__setitem__(key, val)
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,20 +3,81 @@
 import warnings
 
 from google.cloud import ndb
+from google.cloud.ndb.exceptions import BadValueError
 
-from ..models import StringIdModel
+from ..models import AssignableSet, StringIdModel, StringSetProperty, UniqueList
 from .. import appengine_config, testutil
 
 
-class StringIdModelTest(testutil.TestCase):
+class ModelTest(testutil.TestCase):
 
   def setUp(self):
+    super().setUp()
+
     warnings.filterwarnings('ignore', module='google.auth',
       message='Your application has authenticated using end user credentials')
 
-  def test_put(self):
-    with appengine_config.ndb_client.context():
-      self.assertEqual(ndb.Key(StringIdModel, 'x'),
-                       StringIdModel(id='x').put())
-      self.assertRaises(AssertionError, StringIdModel().put)
-      self.assertRaises(AssertionError, StringIdModel(id=1).put)
+    self.ndb_context = appengine_config.ndb_client.context()
+    self.ndb_context.__enter__()
+
+  def tearDown(self):
+    self.ndb_context.__exit__(None, None, None)
+    super().tearDown()
+
+  def test_StringIdModel_put(self):
+    self.assertEqual(ndb.Key(StringIdModel, 'x'),
+                     StringIdModel(id='x').put())
+    self.assertRaises(AssertionError, StringIdModel().put)
+    self.assertRaises(AssertionError, StringIdModel(id=1).put)
+
+  def test_UniqueList(self):
+    l = UniqueList(['x', 'y', 'x'])
+    self.assertCountEqual(['x', 'y'], l)
+
+    l[:] = ['a', 'b', 'a']
+    self.assertIsInstance(l, UniqueList)
+    self.assertCountEqual(['a', 'b'], l)
+
+    l.append('a')
+    l.append('c')
+    l += ['c', 'c', 'd', 'a']
+    l.remove('a')
+    l.extend(['e', 'b'])
+    self.assertIsInstance(l, UniqueList)
+    self.assertCountEqual(['b', 'c', 'd', 'e'], l, l)
+
+  def test_AssignableSet(self):
+    s = AssignableSet(['x', 'y'])
+
+    s[:] = ['a', 'b']
+    self.assertIsInstance(s, AssignableSet)
+    self.assertEqual({'a', 'b'}, s)
+
+    s.add('c')
+    s.remove('a')
+    s |= {'d', 'b'}
+    self.assertIsInstance(s, AssignableSet)
+    self.assertEqual({'b', 'c', 'd'}, s)
+
+  def test_StringSetProperty(self):
+    class Foo(ndb.Model):
+      x = StringSetProperty()
+
+    f = Foo(x=['a', 'b', 'a'])
+    self.assertEqual(['a', 'b'], f.x)
+
+    f = f.put().get()
+    self.assertEqual(['a', 'b'], f.x)
+
+    f.x.add('a')
+    f.x.add('c')
+    f.x.remove('a')
+    f.x.update({'d', 'b'})
+    self.assertEqual(['b', 'c', 'd'], f.x)
+
+    f = f.put().get()
+    self.assertEqual(['b', 'c', 'd'], f.x)
+
+    g = Foo(x=f.x)
+    g = g.put().get()
+    self.assertEqual(f.x, g.x)
