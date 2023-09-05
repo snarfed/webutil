471c8476933c861a1f93fd9b171a399d412c1202
Ryan Barrett <git@ryanb.org>
2023-09-04 19:25:17 -0700

    appengine_info: detect GAE Flex's GAE_INSTANCE env var

--- appengine_info.py
+++ appengine_info.py
@@ -1,6 +1,9 @@
 """HTTP request and server information derived from the environment.
 
-App Engine: https://cloud.google.com/appengine/docs/standard/python3/runtime#environment_variables
+App Engine:
+* https://cloud.google.com/appengine/docs/standard/python3/runtime#environment_variables
+* https://cloud.google.com/appengine/docs/flexible/python/runtime#environment_variables
+
 WSGI: https://www.python.org/dev/peps/pep-3333/
 
 To run against the prod datastore, make a service account, download its JSON
@@ -18,4 +21,5 @@ if creds and not creds.endswith('fake_user_account.json'):
 else:
   APP_ID = os.getenv('GAE_APPLICATION', '').split('~')[-1]
   DEBUG = os.environ.get('GAE_ENV') in (None, 'localdev')  # 'standard' in production
-  LOCAL = os.environ.get('GAE_ENV') == 'localdev'
+  LOCAL = (os.environ.get('GAE_ENV') == 'localdev'  # App Engine Standard
+           and not os.environ.get('GAE_INSTANCE'))  # App Engine Flex
