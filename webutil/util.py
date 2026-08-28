0fadff1f90846da889ec47a5e25be0379dc20c07
Ryan Barrett <git@ryanb.org>
2026-08-28 14:49:15 -0700

    util.requests_fn: demote cache INFO log to DEBUG, add more debug logging
    for #588

--- webutil/util.py
+++ webutil/util.py
@@ -1938,10 +1938,11 @@ def requests_fn(url, fn=None, *args, log_data=True, cache=False, **kwargs):
     # check cache first
     #
     # can't use response's truthiness; Response.__bool__ is False for 4xx and 5xx
+    if cache_key:
+      logger.debug(f'Checking HTTP cache for {cache_key}')
     if cache_key and cache_key in cache_dict:
       cached = cache_dict[cache_key]
-      # TODO: demote to debug
-      logger.info(f'HTTP cache hit for {method} {url}')
+      logger.debug(f'HTTP cache hit for {cache_key}')
       if isinstance(cached, BaseException):
         raise cached
       resp = cached
@@ -1951,11 +1952,13 @@ def requests_fn(url, fn=None, *args, log_data=True, cache=False, **kwargs):
         resp = fn(url, *args, **kwargs)
       except Exception as e:
         if cache_key:
+          logger.debug(f'HTTP Caching exception {e}')
           cache_dict[cache_key] = e
         raise
       # only cache responses we've already read. requests_fn streams by default,
       # so an unread body can't be replayed to a second caller.
       if cache_key and resp._content is not False:
+        logger.debug(f'HTTP caching response {resp}')
         cache_dict[cache_key] = resp
 
     msg = f'Received {resp.status_code} '
